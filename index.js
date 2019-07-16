6e6f19e94401018fa909f1d6e0971b1218566a9a
JC Ivancevich <jc.ivancevich@gmail.com>
2019-07-16 17:47:34 -0300

    move payload log up, and little fixes

--- index.js
+++ index.js
@@ -12,13 +12,21 @@ module.exports = async (req, res) => {
 
   let payload;
   try {
-    payload = await json(req); // gets payload
+    payload = await json(req);
   } catch (e) {
     logger("err", "Missing JSON payload");
     return send(res, 400, "Missing JSON payload");
   }
 
   logger("debug", `Requesting ${pathname}`);
+  logger(
+    "debug",
+    `Payload from docker hub:\n ${JSON.stringify(
+      payload,
+      null,
+      2
+    )} \nRunning hook on repo: ${payload.repository.repo_name}`
+  );
 
   try {
     await validateReq({ pathname, payload, hooks }); // validates token and payload
@@ -28,14 +36,7 @@ module.exports = async (req, res) => {
   }
   // everything is on it's right place...
   send(res, 204); // sends 'no content' to client
-  logger(
-    "debug",
-    `Payload from docker hub:\n ${JSON.stringify(
-      payload,
-      null,
-      2
-    )} \nRunning hook on repo: ${payload.repository.repo_name}`
-  );
+
   const hook = hooks(payload.repository.repo_name, payload.push_data.tag);
 
   try {
